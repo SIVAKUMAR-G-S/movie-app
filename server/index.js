1a124e97d3d7385f6858fe7f949f31567e90aa51
SIVAKUMAR-G-S <sivakumar25828@gmail.com>
2025-03-15 14:26:52 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -19,14 +19,13 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-  console.log("Mongodb connected");
-  server.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
   });
-}).catch((err) => {
-  console.log({ err });
-  process.exit(1);
-});
-
-//test
